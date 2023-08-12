4aae8f1424936569f021d9bce9cf3291f01eb21a
Basit46 <Hassanbasit1990@gmail.com>
2023-08-12 17:28:04 +0100

    Added animation to project-image

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,6 +1,11 @@
+import { useRef, useEffect } from "react";
 import { FiArrowUpRight } from "react-icons/fi";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-type ProjectProp = {
+gsap.registerPlugin(ScrollTrigger);
+
+type ProjectPropType = {
   project: {
     id: number;
     img: string;
@@ -11,14 +16,49 @@ type ProjectProp = {
   };
 };
 
-const Project = ({ project }: ProjectProp) => {
+const Project = ({ project }: ProjectPropType) => {
+  const coverRef: any = useRef();
+  const imgRef: any = useRef();
+
+  useEffect(() => {
+    gsap.to(coverRef.current, {
+      width: 0,
+      duration: 2,
+      scrollTrigger: {
+        trigger: coverRef.current,
+      },
+    });
+  }, []);
+
+  useEffect(() => {
+    gsap.fromTo(
+      imgRef.current,
+      {
+        scale: 1.3,
+      },
+      {
+        scale: 1,
+        duration: 2,
+
+        scrollTrigger: {
+          trigger: coverRef.current,
+        },
+      }
+    );
+  }, []);
+
   return (
     <div className="project w-full h-fit flex flex-col items-start gap-y-[20px]">
-      <div className="w-fit border-[2px] border-white rounded-[10px] overflow-hidden">
+      <div className="w-full relative border-[2px] border-white rounded-[10px] overflow-hidden">
         <img
+          ref={imgRef}
           className="h-full w-full object-fit"
           src={project.img}
           alt="project_img"
+        />
+        <div
+          ref={coverRef}
+          className="absolute top-0 right-0 w-[80%] h-full bg-white"
         />
       </div>
 
